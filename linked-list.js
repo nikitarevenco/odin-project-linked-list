b0808f93444f419efc9366a736c321c6bf235146
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-02-04 14:33:28 +0000

    Add contains method
    Checks if a value is in the linked list

--- linked-list.js
+++ linked-list.js
@@ -3,23 +3,57 @@ class LinkedList {
     this.value = value;
     this.next = next;
   }
+  size(obj = this, counter = 0) {
+    const self = this;
+    if (obj.next === null) {
+      counter++;
+      return counter;
+    } else {
+      return self.size(obj.next, ++counter);
+    }
+  }
+  pop() {
+    this.at(this.size() - 2).next = null;
+  }
+  at(index, counter = 0, obj = this) {
+    const self = this;
+
+    if (index !== counter) {
+      return self.at(index, ++counter, obj.next);
+    }
+
+    return obj;
+  }
+  contains(value, obj = this) {
+    const self = this;
+    if (obj.value === value) {
+      return true;
+    }
+    if (obj.next === null) {
+      return false;
+    } else {
+      return self.contains(value, obj.next);
+    }
+  }
   prepend(value) {
     const current = new LinkedList(this.value, this.next);
     this.next = current;
     this.value = value;
   }
   append(value) {
-    this.getLastElement().next = new Node(value);
+    this.tail().next = new Node(value);
   }
-  getLastElement(obj = this) {
+  head() {
+    return this;
+  }
+  tail(obj = this) {
     const self = this;
     if (obj.next === null) {
       return obj;
     } else {
-      return self.getLastElement(obj.next);
+      return self.tail(obj.next);
     }
   }
-  // append(value) {}
 }
 
 class Node {
@@ -35,8 +69,7 @@ list.prepend("3");
 list.prepend("4");
 list.prepend("5");
 list.append("hello BROO");
-
 list.append("AYOOOOOO LETS GOOOOO");
 // list.append("goodbye");
 // list.append("yo bro");
-console.log(list);
+console.log(list.tail());
